16341c916a3dd14dd3a623495b223b0a2d7f6593
ddmck <ddmckendrick@gmail.com>
2013-04-18 08:59:59 +0100

    form-based workflow feature completed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = '0b0807e489ac40969f0233e0f23a850ae813ab06327a3ef231df3994b0ec40b1f1c5c33629c3c1ce6a7d6a0e076f2e7b71f078dacea7940efa2211e027df30e7'
